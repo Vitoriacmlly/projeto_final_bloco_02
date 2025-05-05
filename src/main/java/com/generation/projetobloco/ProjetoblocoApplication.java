6d3aaf3d2b4954405bc3adf4390d8d67a6c2d5a3
Vitoriacmlly <vcamilly18@gmail.com>
2025-05-05 09:41:14 -0300

    first commit

---
+++ src/main/java/com/generation/projetobloco/ProjetoblocoApplication.java
@@ -0,0 +1,13 @@
+package com.generation.projetobloco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoblocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoblocoApplication.class, args);
+	}
+
+}
